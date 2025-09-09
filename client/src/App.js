e0a2b264823d2f56ed9a6b6592b1bb7e6605e975
chaitanya_06 <153907599+chaitanya210621@users.noreply.github.com>
2025-09-09 08:00:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import LostItems from "./Components/LostItems";
@@ -9,31 +11,40 @@ import Home from "./Components/Home";
 import ItemPage from "./Components/ItemPage";
 import LostItem from "./Components/Lost_item";
 import MyListings from "./Components/MyListings";
-import Layout from "./layout"; 
-window.OneSignal = window.OneSignal || [];
-const OneSignal = window.OneSignal;
+import Layout from "./layout";
+
 function App() {
- 
-  
+
+  // Initialize OneSignal (if using push notifications)
+  useEffect(() => {
+    if (window.OneSignal) {
+      window.OneSignal = window.OneSignal || [];
+      window.OneSignal.push(() => {
+        window.OneSignal.init({
+          appId: "YOUR-ONESIGNAL-APP-ID", // Replace with your OneSignal App ID
+        });
+      });
+    }
+  }, []);
+
   return (
-      <BrowserRouter>
-          <Layout>
-          <Routes>
-
-          <Route path="/" element={<Home />}  />
-          <Route path="/log-in" element={<Login/>} />
-          <Route path="/sign-up" element={<Signup/>} />
-          <Route path="/lostitems" element={<LostItems/>} />
-          <Route path="/founditems" element={<FoundItems/>} />
-          <Route path="/postitem" element={<LostItem/>} />
-          <Route path="/mylistings" element={<MyListings/>} />
-          <Route path="/:item" element={<ItemPage/>} />
-          <Route path="/*" element={<Home/>} />
-          </Routes>
-          <ToastContainer />
-          </Layout>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <Routes>
+        {/* Wrap all routes inside Layout */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />  {/* Home page */}
+          <Route path="log-in" element={<Login />} />
+          <Route path="sign-up" element={<Signup />} />
+          <Route path="lostitems" element={<LostItems />} />
+          <Route path="founditems" element={<FoundItems />} />
+          <Route path="postitem" element={<LostItem />} />
+          <Route path="mylistings" element={<MyListings />} />
+          <Route path="item/:itemId" element={<ItemPage />} /> {/* Dynamic route */}
+          <Route path="*" element={<Home />} /> {/* Fallback */}
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
   );
 }
 
